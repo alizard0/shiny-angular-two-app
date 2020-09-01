ea3afb14c08642944caffd77c63bfd902a2c78e1
Andre Lizardo <andrelizardo@localhost.localdomain>
2020-09-01 14:45:05 +0200

    fifth step

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero.int'
-import { HEROES_DB } from '../mock-heroes-db'
+import { MessageService } from '../message.service';
+import { Hero } from '../hero.int';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +9,23 @@ import { HEROES_DB } from '../mock-heroes-db'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-	heroes = HEROES_DB;
+	heroes: Hero[];
+	selectedHero: Hero;
 
-	constructor() { }
+	constructor(private heroService: HeroService, 
+		private messageService: MessageService) { }
 
-	ngOnInit(): void {
+	getHeroes(): void {
+		this.heroService.getHeroes()
+			.subscribe(heroes => this.heroes = heroes);
 	}
 
-	selectedHero: Hero;
+	ngOnInit(): void {
+		this.getHeroes();
+	}
 
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
+		this.messageService.add('HeroesComponent: Selected hero id=' + hero.id);
 	}
 }
